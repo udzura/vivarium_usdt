f26b91a560f014d41da213a85b809915dfa25ad6
Uchio Kondo <uchio.kondo@smarthr.co.jp>
2026-06-09 21:26:50 +0900

    Remove old test

--- test/vivarium_usdt_test.rb
+++ test/vivarium_usdt_test.rb
@@ -8,23 +8,4 @@ class VivariumUsdtTest < Test::Unit::TestCase
       ::Vivarium::Usdt.const_defined?(:VERSION)
     end
   end
-
-  test "put and get method name" do
-    method_signature = "SomeClass#some_method"
-    method_id = Vivarium::Usdt.register_or_resolve_method(method_signature)
-    assert_equal(method_signature, Vivarium::Usdt.get_method_name(method_id))
-  end
-
-  test "raise stores error and message names" do
-    error_name = "RuntimeError"
-    message = "boom"
-
-    Vivarium::Usdt.raise(error_name, message)
-
-    error_id = __helper_get_hash_from_name(error_name)
-    message_id = __helper_get_hash_from_name(message)
-
-    assert_equal(error_name, Vivarium::Usdt.get_error_name(error_id))
-    assert_equal(message, Vivarium::Usdt.get_message_name(message_id))
-  end
 end
